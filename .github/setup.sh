57575b6fd9d394596e459394f2859d6480b6a436
jrakibi <j.errakibi@gmail.com>
2025-02-27 14:29:49 +0700

    Add caching for Bitcoin Core downloads

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,5 +1,3 @@
-wget https://bitcoincore.org/bin/bitcoin-core-28.0/bitcoin-28.0-x86_64-linux-gnu.tar.gz
-tar -xzvf bitcoin-28.0-x86_64-linux-gnu.tar.gz
 ln -s $PWD/bitcoin-28.0/bin/* /usr/local/bin/
 mkdir -p ~/.bitcoin
 
